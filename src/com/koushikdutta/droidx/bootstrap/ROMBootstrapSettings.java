be11217007dacd242dca3c0612e164b68dc8107a
cvpcs <root@cvpcs.org>
2010-12-29 17:48:19 -0600

    change to JOSN-based config

--- src/com/koushikdutta/droidx/bootstrap/ROMBootstrapSettings.java
+++ src/com/koushikdutta/droidx/bootstrap/ROMBootstrapSettings.java
@@ -1,10 +1,11 @@
 package com.koushikdutta.droidx.bootstrap;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.File;
+import java.io.FileInputStream;
 
 import android.util.Log;
+
+import org.json.JSONObject;
 
 public class ROMBootstrapSettings {
 	private static final String TAG = "DXB/ROMBootstrapSettings";
@@ -22,41 +23,40 @@ public class ROMBootstrapSettings {
 		if(SETTINGS_FILE.exists()) {
 			Log.d(TAG, "Found settings file, parsing");
 			
+			FileInputStream f = null;
+			String data = "";
+			
 			try {
-				BufferedReader br = new BufferedReader(new FileReader(SETTINGS_FILE));
-				
-				String line = null;
-				
-				while((line = br.readLine()) != null) {
-					if(line.length() > 0 && line.charAt(0) == '#') {
-						// ignore # characters
-						continue;
-					}
-					
-					// split on =
-					String[] parts = line.split("=");
-					
-					// ignore malformed shit
-					if(parts.length == 2) {
-						String key = parts[0].trim().toLowerCase();
-						boolean value = Boolean.valueOf(parts[1].trim());
-						
-						if(key.equals(RESTART_ADB_KEY)) {
-							Log.d(TAG, "Setting [RestartAdb] to [" + (value ? "true" : "false") + "]");
-							mRestartAdb = value;
-						} else if(key.equals(INSTALL_HIJACK_KEY)) {
-							Log.d(TAG, "Setting [InstallHijack] to [" + (value ? "true" : "false") + "]");
-							mInstallHijack = value;
-						} else if(key.equals(INSTALL_RECOVERY_KEY)) {
-							Log.d(TAG, "Setting [InstallRecovery] to [" + (value ? "true" : "false") + "]");
-							mInstallRecovery = value;
-						}
-					}
-				}
-				
-				br.close();
+				// read file into a string
+			    byte[] buffer = new byte[(int) SETTINGS_FILE.length()];
+				f = new FileInputStream(SETTINGS_FILE);
+				f.read(buffer);
+				data = new String(buffer);
 			} catch(Exception e) {
-				Log.e(TAG, "Error parsing file", e);
+				Log.e(TAG, "Error reading settings file", e);
+			} finally {
+				// ensure the stream is closed
+				if(f != null) try { f.close(); } catch(Exception ignored) { }
+			}
+			
+			try {
+				// parse as JSON
+				JSONObject json = new JSONObject(data);
+				
+				if(json.has(RESTART_ADB_KEY)) {
+					mRestartAdb = json.optBoolean(RESTART_ADB_KEY, true);
+					Log.d(TAG, "Setting [RestartAdb] to [" + (mRestartAdb ? "true" : "false") + "]");
+				}
+				if(json.has(INSTALL_HIJACK_KEY)) {
+					mInstallHijack = json.optBoolean(INSTALL_HIJACK_KEY, true);
+					Log.d(TAG, "Setting [InstallHijack] to [" + (mInstallHijack ? "true" : "false") + "]");
+				}
+				if(json.has(INSTALL_RECOVERY_KEY)) {
+					mInstallRecovery = json.optBoolean(INSTALL_RECOVERY_KEY, true);
+					Log.d(TAG, "Setting [InstallRecovery] to [" + (mInstallRecovery ? "true" : "false") + "]");
+				}
+			} catch(Exception e) {
+				Log.e(TAG, "Error parsing settings file", e);
 			}
 		}
 	}
